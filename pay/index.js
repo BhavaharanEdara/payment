4d92d89a43d6df6dda5c7511cc36322d23147ce8
Edara.Bhavaharan <89628831+BhavaharanEdara@users.noreply.github.com>
2024-01-20 18:00:39 +0530

    Update index.js

--- pay/index.js
+++ pay/index.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 const cors = require("cors");
 app.use(cors(
     {
-        origin:"https://payment-lcu8.vercel.app"
+        origin:"https://payment-r17t.vercel.app"
     }
 ));
 
